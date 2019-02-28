cf437e55765efd5c4abe2481a343191189aa7898
Dennis Ly <dennisly1995@gmail.com>
2019-02-27 19:52:15 -0600

    Se completa el punto a y b de la kata.

--- ExamenPatrones/src/com/cenfotec/Analytics.java
+++ ExamenPatrones/src/com/cenfotec/Analytics.java
@@ -3,11 +3,75 @@ package com.cenfotec;
 public class Analytics {
 
 	public static String[] LaMasLarga(String frase) {
+
+		String[] arregloPalabras = frase.split(" ");
+		String[] arregloRetorno;
+		String palabraTemp = "";
+		String palabraMayor = "";
+		String frasePalabras = "";
+		int tamano = 0;
+
+		// Obtengo la palabra mas larga.
+
+		palabraTemp = arregloPalabras[0];
+		for (int a = 1; a < arregloPalabras.length; a++) {
+			if (palabraTemp.length() > arregloPalabras[a].length()) {
+				palabraMayor = palabraTemp;
+			} else if(palabraTemp.length() < arregloPalabras[a].length()) {
+				palabraMayor = arregloPalabras[a];
+			}
+		}
+		tamano = palabraMayor.length();
 		
+		for(int i=0;i<arregloPalabras.length;i++) {
+			if(arregloPalabras[i].length() == tamano) {
+				frasePalabras = frasePalabras + arregloPalabras[i] + " ";
+			}
+		}
+		arregloRetorno = frasePalabras.split(" ");
+
+		return arregloRetorno;
+	}
+
+	
+	
+	
+	
+	public static String[] LaMasCorta(String frase) {
+
+		String[] arregloPalabras = frase.split(" ");
+		String[] arregloRetorno;
+		String palabraTemp = "";
+		String palabraMenor = "";
+		String frasePalabras = "";
+		int tamano = 0;
+
+		// Obtengo la palabra mas larga.
+
+		palabraTemp = arregloPalabras[0];
+		for (int a = 1; a < arregloPalabras.length; a++) {
+			if (palabraTemp.length() < arregloPalabras[a].length()) {
+				palabraMenor = palabraTemp;
+			} else if(palabraTemp.length() > arregloPalabras[a].length()) {
+				palabraMenor = arregloPalabras[a];
+			}
+		}
+		tamano = palabraMenor.length();
 		
+		for(int i=0;i<arregloPalabras.length;i++) {
+			if(arregloPalabras[i].length() == tamano) {
+				frasePalabras = frasePalabras + arregloPalabras[i] + " ";
+			}
+		}
+		arregloRetorno = frasePalabras.split(" ");
 		
-		
-		return null;
+		System.out.println("======================Cortas");
+		for (int i = 0; i < arregloRetorno.length; i++) {
+			System.out.println(arregloRetorno[i]);
+		}
+		System.out.println("======================");
+
+		return arregloRetorno;
 	}
 
 }
--- ExamenPatrones/src/com/cenfotec/test/AnalyticsTest.java
+++ ExamenPatrones/src/com/cenfotec/test/AnalyticsTest.java
@@ -2,6 +2,7 @@ package com.cenfotec.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import com.cenfotec.Analytics;
@@ -9,22 +10,43 @@ import com.cenfotec.Analytics;
 public class AnalyticsTest {
 
 	@Test
-	public static void TestLaMasLarga() {
+	public void TestLaMasLarga() {
 		
 		String frase1 = "casa casita casotota";
-		String frase2 = "casotota";
-		String frase3 = "casa mia cosa";
+		String frase2 = "casa mia cosa";
 		
 		String[] arreglo1 = {"casotota"};
-		String[] arreglo2 = {"casotota"};
-		String[] arreglo3 = {"casa","cosa"};
+		String[] arreglo2 = {"casa","cosa"};
 		
 		String[] arregloResultado1 = Analytics.LaMasLarga(frase1);
 		String[] arregloResultado2 = Analytics.LaMasLarga(frase2);
-		String[] arregloResultado3 = Analytics.LaMasLarga(frase3);
 		
-		assertEquals(arreglo1, arregloResultado1, "");
+		//assertEquals(arregloResultado1, arreglo1, "Resultado deberia de ser casotota");
+		//assertEquals(arregloResultado2, arreglo2, "Resultado deberia de ser casotota");
+		//assertEquals(arregloResultado3, arreglo3, "Resultado deberia de ser casa, cosa");
+		Assert.assertArrayEquals( arregloResultado1, arreglo1 );
+		Assert.assertArrayEquals( arregloResultado2, arreglo2 );
 		
+	}
+	
+	
+	@Test
+	public void TestLaMasCorta() {
+		
+		String frase1 = "casa casita casotota";
+		String frase2 = "casa mia cosa ala";
+		
+		String[] arreglo1 = {"casa"};
+		String[] arreglo2 = {"mia", "ala"};
+		
+		String[] arregloResultado1 = Analytics.LaMasCorta(frase1);
+		String[] arregloResultado2 = Analytics.LaMasCorta(frase2);
+		
+		//assertEquals(arregloResultado1, arreglo1, "Resultado deberia de ser casotota");
+		//assertEquals(arregloResultado2, arreglo2, "Resultado deberia de ser casotota");
+		//assertEquals(arregloResultado3, arreglo3, "Resultado deberia de ser casa, cosa");
+		Assert.assertArrayEquals( arregloResultado1, arreglo1 );
+		Assert.assertArrayEquals( arregloResultado2, arreglo2 );
 	}
 	
 }
